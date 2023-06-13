010dda39de3b0303d2f63ca85d01ac826f628ae4
wsoksoda <w.soksoda@gmail.com>
2023-06-13 10:10:46 -0500

    added detail page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import PokemonNavbar from "../src/pokemonNavbar";
 import PokemonCard from "../src/pokemonCard";
-import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import Navbar from "@/navbar";
+import { BrowserRouter } from "react-router-dom";
 
 function Home() {
   let pokemons: pokemon[] = [];
@@ -14,11 +13,7 @@ function Home() {
 
   const [pages, setPages] = React.useState(1);
 
-  // const pageable = useParams();
-
   const router = useRouter();
-
-  console.log(router.query);
 
   const offset = parseInt((router.query.offset as string) ?? "1");
 
@@ -35,20 +30,16 @@ function Home() {
   }, [offset]);
 
   function next() {
-    //todo error handling
     if (offset < pages) {
-      let currentPage = offset + 1;
+      const currentPage = offset + 1;
       router.push(`/?offset=${currentPage}`);
-    } else {
     }
   }
 
   function back() {
     if (offset > 1) {
-      let currentPage = offset - 1;
-
+      const currentPage = offset - 1;
       router.push(`/?offset=${currentPage}`);
-    } else {
     }
   }
 
